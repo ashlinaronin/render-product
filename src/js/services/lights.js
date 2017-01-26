975de733599832ccd93647ea823671344bbae738
ashlinaronin <ashlinaronin@gmail.com>
2017-01-25 16:06:04 -0800

    Update lighting

--- src/js/services/lights.js
+++ src/js/services/lights.js
@@ -1,9 +1,9 @@
 let lightsPromise = new Promise(function(resolve, reject) {
-    const leftPoint = new THREE.PointLight(0xffffff, 1.4);
-    leftPoint.position.set(-10, 5, -2);
+    const leftPoint = new THREE.PointLight(0xffffff, 1.4, 100, 2);
+    leftPoint.position.set(-15, -5, 10);
 
-    const rightPoint = new THREE.PointLight(0xffffff, 1.4);
-    rightPoint.position.set(10, 5, -2);
+    const rightPoint = new THREE.PointLight(0xffffff, 1.4, 100, 2);
+    rightPoint.position.set(15, 5, 10);
 
     resolve([leftPoint, rightPoint]);
 });
